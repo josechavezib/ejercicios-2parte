40cdd629fcbff3bf6dadf767cc383f584579c74e
josechavezib <134570096+josechavezib@users.noreply.github.com>
2023-06-07 15:59:12 -0500

    Update 12.pico y placa jose.py

--- 12.pico y placa jose.py
+++ 12.pico y placa jose.py
@@ -4,13 +4,13 @@ semana = ["lunes", "martes", "miercoles", "jueves", "viernes"]
  
 if placa == 1 or placa == 2:
     print ("tu dia de pico y placa es ", semana[4])
-if placa == 3 or placa == 4:
+elif placa == 3 or placa == 4:
     print ("tu dia de pico y placa es ", semana[0])
-if placa == 5 or placa == 6:
+elif placa == 5 or placa == 6:
     print ("tu dia de pico y placa es ", semana[1])
-if placa == 7 or placa == 8:
+elif placa == 7 or placa == 8:
     print ("tu dia de pico y placa es ", semana[2])
-if placa == 9 or placa == 0:
+elif placa == 9 or placa == 0:
     print ("tu dia de pico y placa es ", semana[3])
 else:
     print("ingra un valor valido")
